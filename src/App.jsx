953ef3106c79a16b01555059167a30c30a72a1bf
micaGallo <micaela.gallo.garcia@gmail.com>
2023-06-28 21:52:54 -0300

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from "react";
 import './App.css'
+import Home from "./pages/home";
+import Contact from "./pages/contact";
+import About from "./pages/about";
+import { Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import { makeStyles } from "@material-ui/core/styles";
 
-function App() {
-  const [count, setCount] = useState(0)
+const useStyles = makeStyles({
+  container: {
+    display: "flex"
+  }
+});
 
+export default function App() {
+  const classes = useStyles();
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={classes.container}>
+      <Sidebar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about"  element={<About />} />
+      </Routes>
+    </div>
+  );
 }
-
-export default App
