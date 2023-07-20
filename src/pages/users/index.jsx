99b3448f12a5c4b02a224ffd1e21390ec7faefb7
micaGallo <micaela.gallo.garcia@gmail.com>
2023-07-19 22:51:10 -0300

    change table by sortTable

--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -1,47 +1,48 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import VisibilityIcon from '@mui/icons-material/Visibility';
 import Header from "../../components/Header";
 import Table from "../../components/Table";
 import "./index.scss";
-import { userColumns, userRows } from "../../datatablesource";
+import { headCells, userRows } from "../../helpers/user/datatablesource";
+import SortTable from "../../components/SortTable"
 
 const Users = () => {
-  const [data, setData] = useState(userRows);
+  const handleAddUser = () => {
+    console.log("hola");
+  }
 
-  const handleDelete = (id) => {
-    setData(data.filter((item) => item.id !== id));
+  const handleDelete = () => {
+    console.log("hola");
   };
 
-  const actionColumn = [
-    {
-      field: "action",
-      headerName: "Action",
-      width: 200,
-      renderCell: (params) => {
-        return (
-          <div className="cellAction">
-            <Button variant="outlined" size="small" startIcon={<VisibilityIcon />} href={`/users/${params.row.id}`}>
-              View
-            </Button>
-          </div>
-        );
-      },
-    },
+  const handleBlock = () => {
+    console.log("hola");
+  };
+
+  const handlePasswordReset = () => {
+    console.log("hola");
+  };
+
+  const handleEnableAdmin = (ids) => {
+    console.log("hola");
+  };
+
+  const menuItems = [
+    {name: 'Delete', action: handleDelete},
+    {name: 'Block', action: handleBlock},
+    {name: 'Password reset', action: handlePasswordReset},
+    {name: 'Enable admin account', action: handleEnableAdmin},
   ];
 
   return(
     <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
       <div className="datatableTitle">
         <Header title="TEAM" subtitle="Managing the Team Members" />
-        <Link to="/users/new" className="link">
-          Add New
-        </Link>
+        <Button variant="outlined" onClick={handleAddUser}>Add User</Button>
       </div>
-      <Table data={data} columns={userColumns} actionColumn={actionColumn} />
+      <SortTable menuItems={menuItems} headCells={headCells} rows={userRows}/>
     </Box>
   );
 };
